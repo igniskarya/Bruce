cd8e6402af44dfd4c7336a27ab56bfe7a271523b
igniskarya <karyaignis@gmail.com>
2025-05-10 05:19:32 +0700

    Update WifiMenu.h

--- src/core/menu_items/WifiMenu.h
+++ src/core/menu_items/WifiMenu.h
@@ -5,7 +5,7 @@
 
 class WifiMenu : public MenuItemInterface {
 public:
-    WifiMenu() : MenuItemInterface("WiFi TES") {}
+    WifiMenu() : MenuItemInterface("WiFi TES1") {}
 
     void optionsMenu(void);
     void drawIcon(float scale);
